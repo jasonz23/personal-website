34767668ba5b356f4d127173d103847df0086997
Jason Zhao <jason.zhao@stnvideo.com>
2022-09-13 12:12:17 -0700

    removed files and structured the project

---
+++ src/pages/main-page/index.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+const MainPage = () => {
+    return (
+        <div className="">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
