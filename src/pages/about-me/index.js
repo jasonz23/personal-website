e81606644007a42a0c43cc6fb09e111ff114b486
Jason Zhao <jason.zhao@stnvideo.com>
2022-09-23 01:17:59 -0700

    added subtitles and descriptions

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -12,8 +12,21 @@ const AboutMe = () => {
                 <pre className="main-page-name-row">   / /\ \ | '_ \ / _ \| | | | __| | |\/| |/ _ \</pre>
                 <pre className="main-page-name-row">  / ____ \| |_) | (_) | |_| | |_  | |  | |  __/</pre>
                 <pre className="main-page-name-row"> /_/    \_\_.__/ \___/ \__,_|\__| |_|  |_|\___|</pre>
+                <div className="main-page-subtitle-body">
+                    <p className="main-page-subtitle">Some info about me</p>
+                    <p className="main-page-instructions">Enter 'help' to see list of commands and locations</p>
+                </div>
                 <p><span className="main-page-command-name">jasonzhao</span>@<span className="main-page-command-location">terminal</span>:$ ~ links</p>
                 <p className="main-page-link" onClick={() => {window.open("/","_self");}}>Main Page</p>
+                <p><span className="main-page-command-name">jasonzhao</span>@<span className="main-page-command-location">terminal</span>:$ ~ aboutme</p>
+                    <div className="about-me-info">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, aspernatur doloribus? Distinctio in commodi, nam voluptatum soluta, a praesentium provident obcaecati porro dolor recusandae modi odio delectus beatae est doloremque!
+                    </div>
+                    <div className="about-me-info">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, aspernatur doloribus? Distinctio in commodi, nam voluptatum soluta, a praesentium provident obcaecati porro dolor recusandae modi odio delectus beatae est doloremque!
+                    </div>
+                
+                
                 <div id="about-me-input-body">
 
                 </div>
--- src/pages/main-page/index.js
+++ src/pages/main-page/index.js
@@ -11,13 +11,22 @@ const MainPage = () => {
                     <pre className="main-page-name-row">  |_  |                       |___  / |                </pre>
                     <pre className="main-page-name-row">    | | __ _ ___  ___  _ __      / /| |__   __ _  ___  </pre>
                     <pre className="main-page-name-row">    | |/ _` / __|/ _ \| '_ \    / / | '_ \ / _` |/ _ \ </pre>
-                    <pre className="main-page-name-row">/\__/ / (_| \__ \ (_) | | | |  / /__| | | | (_| | (_) </pre>
+                    <pre className="main-page-name-row">/\__/ / (_| \__ \ (_) | | | |  / /__| | | | (_| | (_) |</pre>
                     <pre className="main-page-name-row">\____/ \__,_|___/\___/|_| |_|  \____/_| |_|\__,_|\___/ </pre>
+                    <div className="main-page-subtitle-body">
+                        <p className="main-page-subtitle">Third year computer science student at The University of British Columbia</p>
+                        <p className="main-page-instructions">Enter 'help' to see list of commands and locations</p>
+                        <p className="main-page-instructions">Enter 'ctrl + c' to clear the commands</p>
+                    </div>
                     <p><span className="main-page-command-name">jasonzhao</span>@<span className="main-page-command-location">terminal</span>:$ ~ links</p>
-                    <p className="main-page-link" onClick={() => {window.open("https://www.github.com/jasonz23")}}>Linkedln</p>
-                    <p className="main-page-link" onClick={() => {window.open("https://www.linkedin.com/in/jason-zhao-24a5b8233/")}}>Github</p>
-                    <p className="main-page-link" onClick={() => {window.open("/about-me","_self");}}>About Me</p>
-                    <p className="main-page-link" onClick={() => {window.open("/contact-me","_self");}}>Contact Me</p>
+                    <button className="main-page-link" onClick={() => {window.open("https://www.github.com/jasonz23")}}>Linkedln</button>
+                    <br />
+                    <button className="main-page-link" onClick={() => {window.open("https://www.linkedin.com/in/jason-zhao-24a5b8233/")}}>Github</button>
+                    <br />
+                    <button className="main-page-link" onClick={() => {window.open("/about-me","_self");}}>About Me</button>
+                    <br />
+                    <button className="main-page-link" onClick={() => {window.open("/contact-me","_self");}}>Contact Me</button>
+                    <br />
                     <div id="main-page-input-body"></div>
                     <Input location="main-page"/>
                     
