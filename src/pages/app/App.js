01198f1c999efbde60d63a062d74bbd4ee155252
Jason Zhao <jason.zhao@stnvideo.com>
2022-09-23 00:28:21 -0700

    added routes to new pages

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,11 +1,20 @@
 import react from 'react';
+import AboutMe from '../about-me';
 import MainPage from '../main-page/index';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const App = () => {
     return (
-        <div>
-            <MainPage />
-        </div>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />}/>
+                    <Route path="/about-me" element={<AboutMe />} />
+                    
+                </Routes>
+                
+            </BrowserRouter>
+        </>
     )
 
 }
