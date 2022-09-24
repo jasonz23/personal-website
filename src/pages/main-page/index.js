1a0d008933732f579d0fab81bedd771455bb2752
Jason Zhao <jason.zhao@stnvideo.com>
2022-09-24 00:43:44 -0700

    added new button for projects

--- src/pages/main-page/index.js
+++ src/pages/main-page/index.js
@@ -12,8 +12,8 @@ const MainPage = () => {
                     <pre className="main-page-name-row">    | | __ _ ___  ___  _ __      / /| |__   __ _  ___  </pre>
                     <pre className="main-page-name-row">    | |/ _` / __|/ _ \| '_ \    / / | '_ \ / _` |/ _ \ </pre>
                     <pre className="main-page-name-row">/\__/ / (_| \__ \ (_) | | | |  / /__| | | | (_| | (_) |</pre>
-                    <pre className="main-page-name-row">\____/ \__,_|___/\___/|_| |_|  \____/_| |_|\__,_|\___/ </pre>
-                    <div className="main-page-subtitle-body">
+                    <pre className="main-page-name-row">\____/ \__,_|___/\___/|_| |_|  \____/_| |_|\__,_|\___/  v1.0.0</pre>
+                    <div className="main-page-subtitle-body"> 
                         <p className="main-page-subtitle">Third year computer science student at The University of British Columbia</p>
                         <p className="main-page-instructions">Enter 'help' to see list of commands and locations</p>
                         <p className="main-page-instructions">Enter 'ctrl + c' to clear the commands</p>
@@ -24,6 +24,8 @@ const MainPage = () => {
                     <button className="main-page-link" onClick={() => {window.open("https://www.linkedin.com/in/jason-zhao-24a5b8233/")}}>Github</button>
                     <br />
                     <button className="main-page-link" onClick={() => {window.open("/about-me","_self");}}>About Me</button>
+                    <br />
+                    <button className="main-page-link" onClick={() => {window.open("/projects","_self");}}>Projects</button>
                     <br />
                     <button className="main-page-link" onClick={() => {window.open("/contact-me","_self");}}>Contact Me</button>
                     <br />
