88fc6f7b02bd2f1ae48b839db34e8d71e26f89ee
Jason Zhao <jason.zhao@stnvideo.com>
2022-09-24 00:45:14 -0700

    file to store information about projects, need to more to database

---
+++ src/static/info/projects-info.js
@@ -0,0 +1,39 @@
+const projectInfo = [
+    {
+        id: 1,
+        title: "School/ Grade Manager",
+        bio: "Application where you can manage and store classes and grades",
+        link: "https://github.com/jasonz23/School-and-Grade-Manager",
+        github: "https://github.com/jasonz23/School-and-Grade-Manager",
+    },
+    {
+        id:2,
+        title: "To-Do Application",
+        bio: "A to do application built that can store and remove tasks",
+        link: "https://to-do-application-jason.netlify.app/",
+        github: "https://github.com/jasonz23/todo-application",
+    },
+    {
+        id: 3,
+        title: "Data Manager",
+        bio: "A data manager than can manipulate and graph data from CSV files",
+        link: "https://github.com/jasonz23/Data-Manager",
+        github: "https://github.com/jasonz23/Data-Manager",
+    },
+    {
+        id: 4,
+        title: "Movie Search Engine",
+        bio: "A web application that has a movie search engine and is able to store movies",
+        link: "https://moviesearchjason.netlify.app/",
+        github: "https://github.com/jasonz23/movie-search",
+    },
+    {
+        id: 5,
+        title: "Tricity Website",
+        bio: "Resturant website for the Tricity Japanese Salmon Bowl",
+        link: "https://tricity.netlify.app/",
+        github: "https://github.com/frankljin/tricity"
+    }
+];
+
+export default projectInfo;
