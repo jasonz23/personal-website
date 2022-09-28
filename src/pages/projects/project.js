a5e4d82b8d2020490265425da955296a156397cb
Jason Zhao <jason.zhao@stnvideo.com>
2022-09-28 00:19:52 -0700

    added icons that link to github and link and added more styling to project

--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -1,6 +1,8 @@
 import { Modal } from "@mui/material";
 import react, {useState} from "react";
 import Input from "../../components/input";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkIcon from '@mui/icons-material/Link';
 import "./projects.css";
 
 const Project = (props) => {
@@ -20,8 +22,16 @@ const Project = (props) => {
         return (
             <Modal open={showModal} keepMounted style={{display:"flex",justifyContent:"center",alignItems:"center"}} onClose={() => setShowModal(false)}>
                 <div className="project-me-page-modal">
-                    <h1>{props.info.title}</h1>
-                    <p>{props.info.bio}</p>
+                    <div>
+                        <h1>{props.info.title}</h1>
+                        <p>{props.info.bio}</p>
+                    </div>
+                    
+                    <div className="project-info-icons">
+                        <GitHubIcon style={{fontSize:"50px", cursor:"pointer"}} onClick={() => {window.open(props.info.github)}}/>
+                        <LinkIcon  style={{fontSize:"50px", cursor:"pointer"}} onClick={() => {window.open(props.info.link)}}/>
+                    </div>
+                    
                 </div>
             </Modal>
         )
