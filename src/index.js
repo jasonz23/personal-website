3cfec602bb02131097cd23199e603a02748e03f4
Jason Zhao <jason1382311@gmail.com>
2023-02-19 18:08:52 -0800

    add spacing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './pages/app/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <div className="root">
