713a8cededa7d0e9e32c2aafc79111902b445638
Jason Zhao <jason1382311@gmail.com>
2023-02-26 18:15:51 -0800

    add new projects and project info

--- src/static/info/projects-info.js
+++ src/static/info/projects-info.js
@@ -3,6 +3,7 @@ const projectInfo = [
         id: 0,
         title: "Color Palette Generator",
         bio: "Generate a color palette based on a given hex code.",
+        tools: ["React.JS", "TypeScript", "Git", "HTML", "CSS"],
         link: "https://jasonz23.github.io/creative-site/",
         github: "https://github.com/jasonz23/creative-site"
     },
@@ -10,13 +11,15 @@ const projectInfo = [
         id: 1,
         title: "School/ Grade Manager",
         bio: "Application where you can manage and store classes and grades",
+        tools: ["Java", "JUnit", "Git", "Java Swing"],
         link: "https://github.com/jasonz23/School-and-Grade-Manager",
         github: "https://github.com/jasonz23/School-and-Grade-Manager",
     },
     {
-        id:2,
+        id: 2,
         title: "To-Do Application",
         bio: "A to do application built that can store and remove tasks",
+        tools: ["React.JS", "JavaScript", "Git", "HTML", "CSS"],
         link: "https://to-do-application-jason.netlify.app/",
         github: "https://github.com/jasonz23/todo-application",
     },
@@ -24,6 +27,7 @@ const projectInfo = [
         id: 3,
         title: "Data Manager",
         bio: "A data manager than can manipulate and graph data from CSV files",
+        tools: ["Python", "Matplotlib"],
         link: "https://github.com/jasonz23/Data-Manager",
         github: "https://github.com/jasonz23/Data-Manager",
     },
@@ -31,6 +35,7 @@ const projectInfo = [
         id: 4,
         title: "Movie Search Engine",
         bio: "A web application that has a movie search engine and is able to store movies",
+        tools: ["React.JS", "Javascript", "Git", "HTML", "CSS"],
         link: "https://moviesearchjason.netlify.app/",
         github: "https://github.com/jasonz23/movie-search",
     },
@@ -38,8 +43,17 @@ const projectInfo = [
         id: 5,
         title: "Tricity Website",
         bio: "Resturant website for the Tricity Japanese Salmon Bowl",
+        tools: ["React.JS", "JavaScript", "Git", "HTML", "CSS", "Node JS", "Strapi"],
         link: "https://tricity.netlify.app/",
         github: "https://github.com/frankljin/tricity"
+    },
+    {
+        id: 6,
+        title: "Art Blog",
+        bio: "Full Stack website that allows users to add, edit and delete art blog posts.",
+        tools: ["React.JS", "JavaScript", "Git", "HTML", "CSS", "Node JS", "Express", "MongoDB"],
+        link: "https://github.com/jasonz23/Art-Blog",
+        github: "https://github.com/jasonz23/Art-Blog"
     }
 ];
 
